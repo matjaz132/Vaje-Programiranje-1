007e8b1f6aec1317523a0560042fe43b854b82ae
Matjaž Jerman <matjaz@X220.Home>
2018-12-05 18:40:25 +0100

    commit

---
+++ Temperatura/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Temperatura {
+    class Program {
+        static void Main (string[] args) {
+            System.Console.WriteLine ("Vnesi temperaturo: ");
+            int temperatura = Console.ReadLine ();
+            if (temperatura < 5 || temperatura > 100) {
+                System.Console.WriteLine ("Error: Temperatura prevečja/premajhna");
+            }
+            if (temperatura <= 10) {
+                
+            }
+        }
+    }
+}
