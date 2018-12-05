c78f909a386b38c1a25fda452ed6d04db26d3c5b
Matjaž Jerman <matjaz@X220.Home>
2018-12-05 18:32:29 +0100

    commit

---
+++ vaja2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace vaja2 {
+    class Program {
+        static void Main (string[] args) {
+            // prvo število
+            System.Console.WriteLine ("Vnesi stranico a: ");
+            string odgovor = Console.ReadLine ();
+            int a = int.Parse (odgovor);
+            // drugo število
+            Console.WriteLine ("Vnesi stranico b: ");
+            odgovor = Console.ReadLine ();
+            int b = int.Parse (odgovor);
+            // tretje število
+            Console.WriteLine ("Vnesi stranico c: ");
+            odgovor = Console.ReadLine ();
+            int c = int.Parse (odgovor);
+        }
+    }
+}
---
+++ vajaIf/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace vajaIf {
+    class Program {
+        static void Main (string[] args) {
+            // vnosni poziv - input prompt
+            System.Console.WriteLine ("Kateri je tvoj najljubši programski jezik?");
+            string odgovor = Console.ReadLine ();
+            if (odgovor == "C#" || odgovor == "c$") {
+                System.Console.WriteLine ("C# je res najboljši");
+            } else {
+                System.Console.WriteLine(odgovor + " je tudi dober jezik");
+            }
+        }
+    }
+}
